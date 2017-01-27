72e70179182f7e85d9b0f6a164aaecbc79b73361
zfdesign <zlatin.ivanov@just-eat.com>
2017-01-27 12:33:54 +0000

    Part 2 Adding a date string native JavaScript

--- Part2/js/app.js
+++ Part2/js/app.js
@@ -1,7 +1,10 @@
 /// /js/app.js
 // The DOM Native way
 const native = document.getElementById('native');
-native.innerHTML = `<div class="domNative">DOM Native DIV</div>`;
+native.innerHTML = `
+<div class="domNative">DOM Native DIV</div>
+<p>${new Date()}</p>
+`;
 
 // React way
 const reactContainer = document.getElementById('react');
