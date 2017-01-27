e11f7dac040d356b79c22ead00215fae43098203
zfdesign <zlatin.ivanov@just-eat.com>
2017-01-27 12:16:00 +0000

    Part 2 Adding a Node the React way

--- Part2/js/app.js
+++ Part2/js/app.js
@@ -2,3 +2,14 @@
 // The DOM Native way
 const native = document.getElementById('native');
 native.innerHTML = `<div class="domNative">DOM Native DIV</div>`;
+
+// React way
+const reactContainer = document.getElementById('react');
+ReactDOM.render(
+    React.createElement(
+        'div',
+        { className: 'demo' },
+        'React Element'
+    ),
+    reactContainer
+);
