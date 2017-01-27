ec9cf56102220c312d587cedd8735ce3188d99cc
zfdesign <zlatin.ivanov@just-eat.com>
2017-01-27 12:44:48 +0000

    Part 2 DOM tree update Native vs React

--- Part2/js/app.js
+++ Part2/js/app.js
@@ -1,23 +1,34 @@
 /// /js/app.js
-// The DOM Native way
-const native = document.getElementById('native');
-native.innerHTML = `
-<div class="domNative">DOM Native DIV</div>
-<p>${new Date()}</p>
-`;
 
-// React way
-const reactContainer = document.getElementById('react');
-ReactDOM.render(
-    React.createElement(
-        'div',
-        { className: 'demo' },
-        'React Element',
+// Wrap in `render` function and call every 1s
+const render = () => {
+
+    // The DOM Native way
+    const native = document.getElementById('native');
+    native.innerHTML = `
+        <div class="domNative">DOM Native DIV</div>
+        <input type="text" />
+        <p>${new Date()}</p>
+    `;
+
+    // React way
+    const reactContainer = document.getElementById('react');
+    ReactDOM.render(
         React.createElement(
-            "p",
-            null,
-            new Date().toString()
-        )
-    ),
-    reactContainer
-);
+            'div',
+            { className: 'demo' },
+            'React Element',
+            React.createElement("input"),
+            React.createElement(
+                "p",
+                null,
+                new Date().toString()
+            )
+        ),
+        reactContainer
+    );
+};
+
+setInterval(render, 1000);
+// TIP: try typing in the input boxes
+// Inspect the DOM updates in Chrome Dev Tools
